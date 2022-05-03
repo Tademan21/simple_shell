cabb1cd79e38f71c1e0765a26e85ac68239eb7ab
Tademan21 <tadessefufamom@gmail.com>
2022-05-02 23:33:21 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,4 +12,19 @@ int main(int argc, char *argv[])
 
 	printf("%s\n", *argv + 0);
 	return (0);
+#include <stdio.h>
+
+/**
+ * main - prints name and a new line to stdout
+ * @argc: counts arguments passed in
+ * @argv: makes an array of strings of arguments passed in
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+        (void)argc;
+
+        printf("%s\n", *argv + 0);
+        return (0);
+
 
