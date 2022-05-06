7c1fd52ce6992bef7e28118ca5ce1738955bb5f7
Tademan21 <101402625+Tademan21@users.noreply.github.com>
2022-05-06 13:39:39 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,30 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * main - function multiplies two positive numbers.
+ * @argc: is an integer
+ * @argv: is a pointer
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
+	int i, j;
+
+	unsigned long product;
+
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
-
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+
+	product = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", product);
+	return (0);
 }
