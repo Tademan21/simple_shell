79b079bcc9472c5b16404f7428a1e1bd6d817120
Tademan21 <101402625+Tademan21@users.noreply.github.com>
2022-05-24 09:51:40 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,13 +1,22 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
+
 /**
- * free_listint - define function
- * @head: describe argument
+ * free_listint - frees a listint_t list
+ * @head: a pointer to the start of the list
+ * Return: nothing.
  */
 void free_listint(listint_t *head)
 {
+	listint_t *temp;
+
 	if (head == NULL)
 		return;
-	free_listint(head->next);
-	free(head);
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
